ca074f5ddc30dc5c9d18dc0e0e154d495f5fee81
zaid-64 <alazzamzaid85@gmail.com>
2026-06-30 18:53:35 +0300

    Add ISearchable interface and LibraryItem

---
+++ Library Management System/Interfaces/ISearchable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Interfaces
+{
+    public interface ISearchable
+    {
+        bool MatchesSearchQuery(string query);
+    }
+}
---
+++ Library Management System/Models/LibraryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Models
+{
+    public abstract class LibraryItem
+    {
+        public int Id { set; get; }
+        public String Title { set; get; }
+        public DateTime AddedDate { set; get; }
+
+
+        public abstract string GetInfo();
+    }
+    
+}
