bb4f25e8169a7c044564e0d81aee0ad04c129e06
zaid-64 <alazzamzaid85@gmail.com>
2026-06-30 19:16:44 +0300

    Add Book class with inheritance and search logic

---
+++ Library Management System/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using Library_Management_System.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Models
+{
+    public class Book : LibraryItem, ISearchable
+    {
+        public string Author { set; get; }
+        public int Year { set; get; }
+        public string Genre { set; get; }
+
+        public bool IsAvailable { set; get; } = true;
+
+        public override string GetInfo()
+        {
+            string status = IsAvailable ? "Available" : "Borrowed";
+
+            return $"Book ID: {Id} | Title: {Title} | Author: {Author} | Year: {Year} | Genre: {Genre} | Status: {status}";
+
+        }
+
+        public bool MatchesSearchQuery(string query)
+        {
+            string lowerQuery = query.ToLower();
+
+            bool titleMatches = Title != null && Title.ToLower().Contains(lowerQuery);
+            bool authorMatches = Author != null && Author.ToLower().Contains(lowerQuery);
+
+            return titleMatches || authorMatches;
+
+        }
+
+
+
+
+    }
+}
